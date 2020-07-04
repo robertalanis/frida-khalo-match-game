c705f88078a8ac012946d599a27c9a7287bd3e3b
robertalanis <rxalanis@gmail.com>
2020-07-03 19:54:00 -0500

    Updates

---
+++ precache-manifest.1cd175bf2aef820b7a3f55faf1e82fb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9dd1a7dbc25d91f05e299bc5705c172",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fe41447472b028b69623",
+    "url": "/clicky-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5af21471bf1317e01332",
+    "url": "/clicky-game/static/css/main.1ca01e42.chunk.css"
+  },
+  {
+    "revision": "fe41447472b028b69623",
+    "url": "/clicky-game/static/js/2.99ae33a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky-game/static/js/2.99ae33a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af21471bf1317e01332",
+    "url": "/clicky-game/static/js/main.2c6b68ba.chunk.js"
+  },
+  {
+    "revision": "d131f015dfebd43c25e2",
+    "url": "/clicky-game/static/js/runtime-main.16a5700b.js"
+  }
+]);
